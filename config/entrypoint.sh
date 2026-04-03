21596fe268554491667d55e7c7451004db1060a7
rterrazas328 <rickyt328@gmail.com>
2026-04-03 14:45:46 -0400

    Fixed dockerignore files and image to exclude auth files and move them elsewhere, and fixed mounted volume

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -5,28 +5,31 @@ set -e
 if [ -e /run/secrets/laravel_app_key ]; then
   export APP_KEY=$(cat /run/secrets/laravel_app_key)
 fi
+if [ -e /run/secrets/db_database ]; then
+  export DB_DATABASE=$(cat /run/secrets/db_database)
+fi
+if [ -e /run/secrets/db_username ]; then
+  export DB_USERNAME=$(cat /run/secrets/db_username)
+fi
 if [ -e /run/secrets/db_password ]; then
   export DB_PASSWORD=$(cat /run/secrets/db_password)
 fi
+if [ -e /run/secrets/email_domain ]; then
+  export EMAIL_DOMAIN=$(cat /run/secrets/email_domain)
+fi
+if [ -e /run/secrets/email_host ]; then
+  export EMAIL_HOST=$(cat /run/secrets/email_host)
+fi
 if [ -e /run/secrets/email_key ]; then
   export EMAIL_KEY=$(cat /run/secrets/email_key)
+fi
+if [ -e /run/secrets/email_username ]; then
+  export EMAIL_USERNAME=$(cat /run/secrets/email_username)
 fi
 if [ -e /run/secrets/email_password ]; then
   export EMAIL_PASSWORD=$(cat /run/secrets/email_password)
 fi
 
-
-#echo "Waiting for MySQL to be ready..."
-# Wait until MySQL responds
-#until php -r "
-#\$mysqli = new mysqli(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
-#if (\$mysqli->connect_errno) { exit(1); }
-#" >/dev/null 2>&1; do
-#  
-#  php -r 'echo "host: " . getenv("MYSQL_USER");'
-#  echo "MySQL is unavailable - sleeping"
-#  sleep 2
-#done
 
 
 echo "MySQL is up - running migrations..."
